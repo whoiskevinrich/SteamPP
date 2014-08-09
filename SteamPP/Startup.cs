7b506514aaad05cc9f97590b2f8aa80d065b460c
whoiskevinrich <whoiskevinrich@gmail.com>
2014-08-09 15:14:31 -0700

    Initial

---
+++ SteamPP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SteamPP.Startup))]
+namespace SteamPP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
